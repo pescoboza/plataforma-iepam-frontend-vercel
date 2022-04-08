4f0bedd4614fcfce9a3dd043331e5aa9ec30901c
Pedro Escoboza <psebastian01@hotmail.com>
2022-04-08 05:02:42 -0500

    :bug: Fix abs paths

--- src/pages/test/gql-client.tsx
+++ src/pages/test/gql-client.tsx
@@ -4,13 +4,15 @@ import { useEmpresasQuery } from "@/codegen/client";
 const Page: FC = () => {
     const { data, error, loading } = useEmpresasQuery();
     if (error) console.error(error);
-    
-    return <div>
-        <h1>Query Hook</h1>
-        {loading && <span>Cargando...</span>}
-        {error && <p>{error.message}</p>}
-        <pre>{JSON.stringify(data, null,4)}</pre>
-    </div>
+
+    return (
+        <div>
+            <h1>Query Hook</h1>
+            {loading && <span>Cargando...</span>}
+            {error && <p>{error.message}</p>}
+            <pre>{JSON.stringify(data, null, 4)}</pre>
+        </div>
+    );
 };
 
 export default Page;
