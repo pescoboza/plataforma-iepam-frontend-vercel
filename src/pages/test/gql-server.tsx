d8288b291e658cda3c86199a9dd1eb557111da44
Pedro Escoboza <psebastian01@hotmail.com>
2022-04-08 05:36:24 -0500

    :rocket: Ready to deploy

--- src/pages/test/gql-server.tsx
+++ src/pages/test/gql-server.tsx
@@ -2,19 +2,19 @@ import type { FC } from "react";
 import type { GetServerSideProps } from "next";
 import { sdk } from "@/codegen/server";
 
-
 export const getServerSideProps: GetServerSideProps = async () => {
     const res = await sdk.Empresas();
 
     return { props: res };
 };
 
-
 const Page: FC = (props) => {
-    return <div>
-        <h1>Server Side Props</h1>
-        <pre>{JSON.stringify(props, null, 4)}</pre>
-    </div>
+    return (
+        <div>
+            <h1>Server Side Props</h1>
+            <pre>{JSON.stringify(props, null, 4)}</pre>
+        </div>
+    );
 };
 
 export default Page;
