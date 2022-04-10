02114eb7b710e30eaf81f1ed9f931fdcef1c1458
Pedro Escoboza <pescoboza@gmail.com>
2022-04-10 00:13:20 +0000

    :wrench: Web config for gzip reverse proxy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps : [{
       name   : "frontend",
-      script : "C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js",
-      args: "run start",
+      script : "./node_modules/next/dist/bin/next",
+      args: "start",
       error_file: './logs/service_gateway.err',
       log_date_format: 'YYYY-MM-DD HH:mm:ss SSS',
       exec_mode: "cluster",
