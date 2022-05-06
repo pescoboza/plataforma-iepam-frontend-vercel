d628f0b075c161eb0fc41fb92e1363f3df09702e
Pedro Escoboza <psebastian01@hotmail.com>
2022-05-06 02:28:39 -0500

    :poop: Harcoded API urls to make to make it work

--- src/lib/codegen/server/index.ts
+++ src/lib/codegen/server/index.ts
@@ -1,10 +1,10 @@
-import { GraphQLClient } from "graphql-request";
-import { getSdk } from "./schema";
-import { GQL_ENDPOINT, GQL_AUTH_TOKEN } from "@/env/server";
+import { GraphQLClient } from 'graphql-request';
+import { getSdk } from './schema';
+import { GQL_ENDPOINT, GQL_AUTH_TOKEN } from '@/env/server';
 
 /**
  * Serverside admin-authenticated requests.
  */
 export const sdk = getSdk(new GraphQLClient(GQL_ENDPOINT, { headers: { Authorization: `Bearer ${GQL_AUTH_TOKEN}` } }));
 
-export * from "./schema";
+export * from './schema';
