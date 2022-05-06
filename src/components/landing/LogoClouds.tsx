ff66d42ace8ffc0761e9e4246670cc9be3ae5f55
armando <a01570376@itesm.mx>
2022-05-05 21:42:21 -0500

    imagenes landing

--- src/components/landing/LogoClouds.tsx
+++ src/components/landing/LogoClouds.tsx
@@ -4,7 +4,7 @@ export default function LogoClouds() {
         <div className="bg-gray-100">
             <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
                 <p className="text-center text-base font-semibold uppercase tracking-wider text-gray-600">
-                    Empleadores Participantes
+                    Algunos de los Empleadores Participantes
                 </p>
                 <div className="mt-6 grid grid-cols-2 gap-4 md:grid-cols-3 lg:mt-8">
                     {companies.map((company) => (
@@ -23,10 +23,10 @@ export default function LogoClouds() {
 }
 
 const companies = [
-    { name: "Chrysler", image: "chrysler" },
-    { name: "MercadoLibre", image: "mercadoLibre" },
-    { name: "Mercedes Benz", image: "mercedesBenz" },
-    { name: "Volkswagen", image: "volkswagen" },
-    { name: "Nissan", image: "nissan" },
-    { name: "Toyota", image: "toyota" },
+    { name: "Vips", image: "vips" },
+    { name: "Sanborns", image: "sanborns" },
+    { name: "Pf Changs", image: "pf-changs-logo" },
+    { name: "Sorteos Tec", image: "sorteostec" },
+    { name: "Burger King", image: "buger-king" },
+    { name: "Starbucks", image: "starbucks" },
 ];
