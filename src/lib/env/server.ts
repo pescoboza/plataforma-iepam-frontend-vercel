036a5f24af69e870fb2f6a13f28d0d9a9c0d56f6
Pedro Escoboza <psebastian01@hotmail.com>
2022-05-06 02:29:47 -0500

    :poop: Harcoded API urls to make to make it work

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -1,7 +1,7 @@
 export const GQL_AUTH_TOKEN = process.env.GQL_AUTH_TOKEN ?? throwOnMissingEnv('GQL_AUTH_TOKEN');
 // export const GQL_ENDPOINT = process.env.GQL_ENDPOINT ?? throwOnMissingEnv("GQL_ENDPOINT");
 // @TODO: Remove this hardoced URL and use the env var instead!!!
-export const GQL_ENDPOINT = 'https://admins.iepam.estetoscopiando.com/grahpql';
+export const GQL_ENDPOINT = 'https://admins.iepam.estetoscopiando.com/graphql';
 
 function throwOnMissingEnv(varName: string) {
     throw new Error(`Missing env var ${varName}`);
