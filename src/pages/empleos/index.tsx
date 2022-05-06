bbaeb879c6aee9aa7d38a4621a4d4afd86f67497
Pedro Escoboza <psebastian01@hotmail.com>
2022-05-06 00:52:35 -0500

    :beer: Help

--- src/pages/empleos/index.tsx
+++ src/pages/empleos/index.tsx
@@ -352,7 +352,9 @@ const JobModal: FC<{
                                         <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
                                             <div className="sm:col-span-1">
                                                 <dt className="text-sm font-medium text-gray-500">Empresa</dt>
-                                                <dd className="mt-1 text-sm text-gray-900">{puesto.empresa.razon}</dd>
+                                                <dd className="mt-1 text-sm text-gray-900">
+                                                    {puesto.empresa.razonSocial}
+                                                </dd>
                                             </div>
                                             <div className="sm:col-span-1">
                                                 <dt className="text-sm font-medium text-gray-500">Vacante</dt>
