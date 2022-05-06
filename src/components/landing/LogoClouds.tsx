813e2597dab96cc62b24650f4a67b3b8a89003ba
armando <a01570376@itesm.mx>
2022-05-05 21:58:04 -0500

    image size and botón CTA

--- src/components/landing/LogoClouds.tsx
+++ src/components/landing/LogoClouds.tsx
@@ -6,11 +6,11 @@ export default function LogoClouds() {
                 <p className="text-center text-base font-semibold uppercase tracking-wider text-gray-600">
                     Algunos de los Empleadores Participantes
                 </p>
-                <div className="mt-6 grid grid-cols-2 gap-4 md:grid-cols-3 lg:mt-8">
+                <div className="mt-6 grid grid-cols-2 gap-4 lg:grid-cols-3 lg:mt-8">
                     {companies.map((company) => (
                         <div className="col-span-1 flex justify-center bg-white py-8 px-8" key={company.image}>
                             <img
-                                className="max-h-12"
+                                className="object-fill"
                                 src={`/images/companies/${company.image}.svg`}
                                 alt={company.name}
                             />
